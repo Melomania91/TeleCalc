d958b23643c6f4f829c323acea61ce5d0aa7fd9f
Melomania <melomania91@gmail.com>
2018-04-25 22:41:14 +0400

    Добавлен вывод истории операций на форму

--- ITUniver.TeleCalc.Web/Controllers/CalcController.cs
+++ ITUniver.TeleCalc.Web/Controllers/CalcController.cs
@@ -2,6 +2,7 @@
 using ITUniver.TeleCalc.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -76,6 +77,49 @@ namespace ITUniver.TeleCalc.Web.Controllers
 
         }
 
-        
+        [HttpGet]
+        public ActionResult OperationHistory()
+        {
+            var history = ReadOrderData(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\ituniver\TeleCalc\ITUniver.TeleCalc.Web\App_Data\TeleCalc.mdf;Integrated Security=True");
+
+            return View(history);
+        }
+
+        private List<OperationHistory> ReadOrderData(string connectionString)
+        {
+            var hist = new List<OperationHistory>();
+            string queryString = "SELECT his.Id, us.Name, op.Name, his.Args, " +
+                "his.Result, his.CalcDate, his.Time " +
+                "FROM dbo.History as his, [dbo].[User] as us, dbo.Operation as op " +
+                "WHERE his.Initiator = us.Id AND his.Operation = op.Id;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Call Read before accessing data.
+                while (reader.Read())
+                {
+                    hist.Add(new OperationHistory()
+                    {
+                        Id = (int)reader[0],
+                        Initiator = (string)reader[1],
+                        Operation = (string)reader[2],
+                        Args = (string)reader[3],
+                        Result = (double)reader[4],
+                        CalcDate = (DateTime)reader[5],
+                        Time = (int)reader[6]
+                    });
+                    
+                }
+
+                // Call Close when done reading.
+                reader.Close();
+                return hist;
+            }
+        }
     }
 }
---
+++ ITUniver.TeleCalc.Web/Models/OperationHistory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ITUniver.TeleCalc.Web.Models
+{
+    public class OperationHistory
+    {
+        public int Id { get; set; }
+
+        public string Initiator { get; set; }
+
+        public string Operation { get; set; }
+
+        public string Args { get; set; }
+
+        public double Result { get; set; }
+
+        public DateTime CalcDate { get; set; }
+
+        public int Time { get; set; }
+    }
+}
